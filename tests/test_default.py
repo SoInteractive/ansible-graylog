0f4b3f9f3ec713427e04d616fa7cc70051b9a8b5
Szymon Zyguła <szygula@sointeractive.pl>
2018-01-23 14:17:17 +0100

    Containerization

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,7 +5,7 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 def test_directories(File):
     present = [
-        "/etc/graylog/server"
+        "/usr/share/graylog-plugins"
     ]
     if present:
         for directory in present:
@@ -14,47 +14,11 @@ def test_directories(File):
             assert d.exists
 
 
-def test_files(File):
-    present = [
-       "/etc/graylog/server/log4j2.xml",
-       "/etc/graylog/server/node-id",
-       "/etc/graylog/server/server.conf",
-       "/usr/share/graylog-server/plugin/graylog-plugin-slack-2.4.0.jar",
-       "/usr/share/graylog-server/plugin/metrics-reporter-prometheus-1.5.0.jar"
-    ]
-    if present:
-        for file in present:
-            f = File(file)
-            assert f.exists
-            assert f.is_file
-
-
-def test_service(Service):
-    present = [
-        "graylog-server"
-    ]
-    if present:
-        for service in present:
-            s = Service(service)
-            assert s.is_enabled
-            assert s.is_running
-
-
 def test_packages(Package):
     present = [
-        "graylog-server"
+        "python-pip"
     ]
     if present:
         for package in present:
             p = Package(package)
             assert p.is_installed
-
-
-def test_socket(Socket):
-    present = [
-        # "unix:///run/haproxy/admin.sock",
-        "tcp://127.0.0.1:9000"
-    ]
-    for socket in present:
-        s = Socket(socket)
-        assert s.is_listening
